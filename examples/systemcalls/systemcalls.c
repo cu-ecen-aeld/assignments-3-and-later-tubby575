5c5bcb703da24c8c66cdce451c6e83516653604d
tubby575 <tubbys575@gmail.com>
2023-05-23 10:22:37 -0600

    WIP, two of three functions working

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,14 @@
 #include "systemcalls.h"
 
+#include <stdlib.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+
+#include <errno.h>
+#include <string.h>
+#include <syslog.h>
+#include <signal.h>
+
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -16,6 +25,35 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+
+    int system_result = system(cmd);
+
+    /** cmd was NULL and system() returned 0 indicating no shell available */
+    if(cmd == NULL && system_result == 0)
+    {
+        return false;
+    }
+
+    /** Only system() return value that is explicitly called an error in man pages, error creating or retrieving child process */
+    if(system_result == -1)
+    {
+        return false;
+    }
+
+    /** Child execution error check*/
+    bool child_res = WIFEXITED(system_result);
+    if(child_res == true)
+    {
+        int child_exit_code = WEXITSTATUS(system_result);
+        if(child_exit_code == 127)
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
 
     return true;
 }
@@ -36,6 +74,8 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+    printf("\nStarting do_exec() on pid = %d\n", getpid());
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -45,9 +85,8 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+
+    va_end(args);
 
 /*
  * TODO:
@@ -59,9 +98,65 @@ bool do_exec(int count, ...)
  *
 */
 
-    va_end(args);
+    printf("\nForking on PID = %d\n", getpid());
+    fflush(stdout);
 
-    return true;
+    int execv_res = 0;
+    int status;
+    pid_t pid;
+    pid = fork();
+
+    if(pid == -1)
+    {
+        printf("\nFailed fork\n");
+        return false;
+    }
+    else if(pid == 0)
+    {
+        char** rem_cmd = (char**)&command[1];
+        printf("\nRunning Exec in child with PID = %d and command = %s %s %s\n", getpid(), command[0], rem_cmd[0], rem_cmd[1]);
+        for(int i = 0; i < count;i++)
+        {
+            printf("\nrem_cmd[%d] = %s", i , rem_cmd[i]);
+        }
+        execv_res = execv(command[0],command);
+        if(execv_res == -1)
+        {
+            printf("\nExec on PID = %d fail with %s\n" , getpid(), strerror(errno));
+            kill(getpid(),SIGINT);
+            return false;
+        }
+    }
+    
+    pid_t wait_res = waitpid(pid, &status, 0);
+    if(wait_res == -1)
+    {
+        printf("\nWaitpid was -1!\n");
+        return false;
+    }
+    else if(WIFEXITED(status) == true)
+    {
+        printf("\nStatus code on PID = %d is %d\n", getpid(), status);
+        int exit_code = WEXITSTATUS(status);
+        if(exit_code != 0)
+        {
+            printf("\nExec Fail on WEXITSTATUS with exit code = %d on PID = %d\n", exit_code, getpid());
+            return false;
+        }
+        else
+        {
+            printf("\nExec Success on WEXITSTATUS with exit code = %d on PID = %d\n", exit_code, getpid());
+            return true;
+        }
+    }
+    else
+    {
+        printf("\nExec Fail on WIFEXITED with status = %d on PID = %d\n", status, getpid());
+        return false;
+    }
+
+    printf("\nGot to end on PID = %d ...\n", getpid());
+    return false;
 }
 
 /**
