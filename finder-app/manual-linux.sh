aadf3113ed5b27e66ec1c92b72ee0abae8959d4b
tubby575 <tubbys575@gmail.com>
2023-05-28 21:07:32 -0600

    Complete Assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,7 +34,25 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Clean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    #set up default config
+    echo "Setting up default .config file..."
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    #build vmlinux target
+    echo "Building vmlinux target...."
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    #Modules and device tree
+    echo "Building modules..."
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+
+    echo "Building device tree..."
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
+    cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 fi
 
 echo "Adding the Image in outdir"
@@ -47,7 +65,18 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+if [ -d "${OUTDIR}/rootfs" ]
+then
+    cd ${OUTDIR}/rootfs
+    mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+    mkdir -p usr/bin usr/lib usr/sbin
+    mkdir -p var/log
+else
+    echo "Failed to create rootfs!"
+    exit -1;
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +84,70 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+echo "Make and install busybox..."
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+TOOL_CHAIN_PATH=$(${CROSS_COMPILE}gcc -print-sysroot)
+echo "Add lib dependencies to rootfs..."
+echo "Dependencies found at ${TOOL_CHAIN_PATH}"
 
-# TODO: Make device nodes
+cp ${TOOL_CHAIN_PATH}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib64
+cp ${TOOL_CHAIN_PATH}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
 
-# TODO: Clean and build the writer utility
+cp ${TOOL_CHAIN_PATH}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${TOOL_CHAIN_PATH}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+cp ${TOOL_CHAIN_PATH}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${TOOL_CHAIN_PATH}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib
+
+cp ${TOOL_CHAIN_PATH}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+cp ${TOOL_CHAIN_PATH}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib
+
+
+# Make device nodes
+echo "Make device nodes..."
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
+
+# Clean and build the writer utility
+echo "Clean and build the writer utility..."
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "Copy finder related scripts..."
+cp writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+cp -r conf/ ${OUTDIR}/rootfs/home
 
-# TODO: Chown the root directory
+# Chown the root directory
+echo "Chown rootfs..."
+sudo chown root:root -R ${OUTDIR}/rootfs
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+echo "Create initramfs.cpio.gz..."
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
+
+echo "Build complete!"
